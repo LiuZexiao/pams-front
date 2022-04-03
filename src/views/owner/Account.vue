8b45d241aa110576a5ea9f50868fd72b331f9aff
LiuZexiao <1710571698@qq.com>
2022-04-04 00:41:38 +0800

    账户中心

--- src/views/owner/Account.vue
+++ src/views/owner/Account.vue
@@ -1,8 +1,29 @@
 <template>
-  账号信息
-  修改密码
-  绑定邮件
-  绑定手机
+  <el-row :gutter="15"
+          style="margin: 3% 12% 0 12%">
+    <!-- 个人信息 -->
+    <el-col :span="8">
+      <el-card shadow="hover"
+               style="height: 110px">
+          <img class="header-logo" src="../../assets/dh1024.png" alt="logo" />
+      </el-card>
+      <!-- 导航 -->
+      <el-card shadow="hover"
+               class="box-card "
+               style="margin-top: 6% ">
+
+      </el-card>
+    </el-col>
+    <!--具体信息 -->
+    <el-col :span="16">
+
+      <el-card shadow="hover"
+               style="height: 210px">
+
+      </el-card>
+    </el-col>
+  </el-row>
+
 </template>
 
 <script>
